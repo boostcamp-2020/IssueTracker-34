ee3f23e7f3f9cf9230c912828179a9f57cedfd1d
rockpell <aythffk@gmail.com>
2020-10-26 18:35:40 +0900

    [Feat] webpack, babel
    add webpack, babel

---
+++ front/webpack.config.js
@@ -0,0 +1,48 @@
+const port = process.env.PORT || 3000;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+    open: true,
+  },
+};
