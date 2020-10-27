263d73fed7315aef8ea5ca71541f2afe0432e20e
gyim1345 <gyim1345@gmail.com>
2020-10-28 00:29:57 +0900

    [Feat] Filter Button
    Add filter Button
    Add filter lists to show on button click
    Add css to filter lists
    Add svg icon to appropriate filter

---
+++ front/src/components/FilterButton.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import LinkExternal from '../svgs/LinkExternal'
+
+const DropDownIcon = styled.span`
+  display: inline-block;
+  width: 0;
+  height: 0;
+  vertical-align: middle;
+  content: "";
+  border-top-style: solid;
+  border-top-width: 4px;
+  border-right: 4px solid transparent;
+  border-bottom: 0 solid transparent;
+  border-left: 4px solid transparent;
+`;
+
+const Button = styled.button`
+    outline: none;
+    cursor: pointer;
+    border: 1px solid;
+    border-radius: 6px;
+`;
+
+const FilterList = styled.section`
+    margin: 8px 0 16px;
+    font-size: 10px;
+    border-color: black;
+    border-radius: 6px;
+    box-shadow: var(--color-shadow-large);
+    border: 1px solid black;
+    position: absolute;
+    background: white;
+    width: 12rem;
+`;
+
+const Div = styled.div`
+  padding: 3px 10px;
+`
+
+const Div2 = styled.div`
+  font-weight: 600;
+  padding: 3px 10px;
+  display: flex;
+  align-items: center;
+`
+
+const Line = styled.div`
+  border-bottom: 1px solid grey;
+
+`
+const X = styled.span`
+  margin-left: auto;
+`;
+
+const FilterButton = ({setFilters}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openFilterList = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const filterIssues = (filterType) => {
+    console.log('filterType', filterType)
+    console.log(LinkExternal)
+    //TODO: setFilters(filterType)
+  }
+
+  return (
+    <div>
+      <Button type="button" onClick={openFilterList} >
+        Filters
+        <DropDownIcon></DropDownIcon>
+      </Button>
+      {isOpen 
+      ? (<FilterList>
+          <Div2>
+            Filter issues
+            <X onClick={()=>openFilterList()}>X</X>
+          </Div2>
+          <Line />
+          <Div onClick={()=>filterIssues('Open issues and pull requests')}>
+            Open issues and pull requests
+          </Div>
+          <Line />
+          <Div onClick={()=>filterIssues('Your issues')}>
+            Your issues
+          </Div>
+          <Line />
+          <Div onClick={()=>filterIssues('Your pull requests')}>
+            Your pull requests
+          </Div>
+          <Line />
+          <Div onClick={()=>filterIssues('Everything assinged to you')}>
+            Everything assigned to you
+          </Div>
+          <Line />
+          <Div onClick={()=>filterIssues('Everything mentioning you')}>
+            Everything mentioning you
+          </Div>
+          <Line />
+          <Div2 onClick={()=>filterIssues('View advanced search syntax')}>
+            <LinkExternal />  View advanced search syntax
+          </Div2>
+      </FilterList>): <></> }
+
+    </div>
+  )
+}
+
+
+export default FilterButton;
+
