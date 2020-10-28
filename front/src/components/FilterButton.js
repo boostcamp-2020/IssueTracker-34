627e286eb26ab26dab2da035e3cb30c0ca298f0e
gyim1345 <gyim1345@gmail.com>
2020-10-28 10:30:20 +0900

    [Feat] filter  close feature
    Add overlay to close filter list

--- front/src/components/FilterButton.js
+++ front/src/components/FilterButton.js
@@ -16,22 +16,29 @@ const DropDownIcon = styled.span`
 `;
 
 const Button = styled.button`
-    outline: none;
-    cursor: pointer;
-    border: 1px solid;
-    border-radius: 6px;
+  outline: none;
+  cursor: pointer;
+  border: 1px solid;
+  border-radius: 6px;
+  border-color: var(--color-btn-border);
+  box-shadow: var(--color-btn-shadow),var(--color-btn-shadow-highlight);
+  background-color: var(--color-btn-bg);
+  &:hover {
+        background-color: #f6f8fa;
+        color: black;
+    }
 `;
 
 const FilterList = styled.section`
-    margin: 8px 0 16px;
-    font-size: 10px;
-    border-color: black;
-    border-radius: 6px;
-    box-shadow: var(--color-shadow-large);
-    border: 1px solid black;
-    position: absolute;
-    background: white;
-    width: 12rem;
+  margin: 8px 0 16px;
+  font-size: 10px;
+  border-color: black;
+  border-radius: 6px;
+  box-shadow: var(--color-shadow-large);
+  border: 1px solid black;
+  position: absolute;
+  background: white;
+  width: 12rem;
 `;
 
 const Div = styled.div`
@@ -53,56 +60,66 @@ const X = styled.span`
   margin-left: auto;
 `;
 
+const DropDownOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
 const FilterButton = ({setFilters}) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const openFilterList = () => {
-    setIsOpen(!isOpen);
-  }
-
-  const filterIssues = (filterType) => {
-    console.log('filterType', filterType)
-    console.log(LinkExternal)
-    //TODO: setFilters(filterType)
-  }
+const openFilterList = () => {
+  setIsOpen(!isOpen);
+}
+const filterIssues = (filterType) => {
+  //TODO: setFilters(filterType)
+}
 
   return (
     <div>
       <Button type="button" onClick={openFilterList} >
         Filters
-        <DropDownIcon></DropDownIcon>
+        <DropDownIcon />
       </Button>
       {isOpen 
-      ? (<FilterList>
-          <Div2>
-            Filter issues
-            <X onClick={()=>openFilterList()}>X</X>
-          </Div2>
-          <Line />
-          <Div onClick={()=>filterIssues('Open issues and pull requests')}>
-            Open issues and pull requests
-          </Div>
-          <Line />
-          <Div onClick={()=>filterIssues('Your issues')}>
-            Your issues
-          </Div>
-          <Line />
-          <Div onClick={()=>filterIssues('Your pull requests')}>
-            Your pull requests
-          </Div>
-          <Line />
-          <Div onClick={()=>filterIssues('Everything assinged to you')}>
-            Everything assigned to you
-          </Div>
-          <Line />
-          <Div onClick={()=>filterIssues('Everything mentioning you')}>
-            Everything mentioning you
-          </Div>
-          <Line />
-          <Div2 onClick={()=>filterIssues('View advanced search syntax')}>
-            <LinkExternal />  View advanced search syntax
-          </Div2>
-      </FilterList>): <></> }
+      ? (
+        <>
+        <FilterList>
+            <Div2>
+              Filter issues
+              <X onClick={()=>openFilterList()}>X</X>
+            </Div2>
+            <Line />
+            <Div onClick={()=>filterIssues('Open issues and pull requests')}>
+              Open issues and pull requests
+            </Div>
+            <Line />
+            <Div onClick={()=>filterIssues('Your issues')}>
+              Your issues
+            </Div>
+            <Line />
+            <Div onClick={()=>filterIssues('Your pull requests')}>
+              Your pull requests
+            </Div>
+            <Line />
+            <Div onClick={()=>filterIssues('Everything assinged to you')}>
+              Everything assigned to you
+            </Div>
+            <Line />
+            <Div onClick={()=>filterIssues('Everything mentioning you')}>
+              Everything mentioning you
+            </Div>
+            <Line />
+            <Div2 onClick={()=>filterIssues('View advanced search syntax')}>
+              <LinkExternal />  View advanced search syntax
+            </Div2>
+          </FilterList>
+          <DropDownOverlay onClick={()=>openFilterList()} />
+          </>
+      ): <></> }
 
     </div>
   )
