ef42d113e5edad26b3336481a5f069e936af8602
rockpell <aythffk@gmail.com>
2020-11-04 03:28:19 +0900

    [Feat] createLabel in model
    add createLabel function

---
+++ back/src/models/label.model.js
@@ -0,0 +1,17 @@
+const { model } = require('../sequelizeModels/config.sequelizeModels');
+const Label = require('../sequelizeModels/label.sequelizeModel');
+
+const createLabel = async ({ name, color, content }) => {
+  try {
+    return await Label.create({
+      name: name,
+      color: color,
+      content: content,
+    });
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+module.exports = { createLabel };
