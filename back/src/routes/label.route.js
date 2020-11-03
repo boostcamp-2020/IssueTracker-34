ff9c9ef252cb618ced67ba21065ddb9cfac931e3
gyim1345 <gyim1345@gmail.com>
2020-11-04 08:50:15 +0900

    [Feat] createLabel route
    Add route to POST /label

--- back/src/routes/label.route.js
+++ back/src/routes/label.route.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const labelController = require('../controllers/label.controller');
+
 router.get('/', (req, res) => {
   res.send('label');
 });
+
+router.post('/label', labelController.createLabel);
 
 module.exports = router;
