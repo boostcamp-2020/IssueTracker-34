6c590aa1adcb55f87328b5b5d9f0a76bfb6a16f5
gyim1345 <gyim1345@gmail.com>
2020-11-04 08:38:23 +0900

    [Feat] getLabel controller
    Add controller for getLabel

---
+++ back/src/controllers/label.controller.js
@@ -0,0 +1,14 @@
+const transactionService = require('../services/label.service');
+
+const getLabels = async (req, res) => {
+  try {
+    const labels = await transactionService.getLabels();
+    return res.status(200).json(labels);
+  } catch (err) {
+    return res.status(500).json({ message: 'Internal Error' });
+  }
+}
+
+module.exports = {
+  getLabels,
+}
