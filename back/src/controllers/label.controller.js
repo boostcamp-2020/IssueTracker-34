dc49503fbb9c6f29da44091cb57326163920bda2
pieisland <pieisland@naver.com>
2020-11-04 12:54:44 +0900

    [Refactor] Refactor label.controller.js
    Change function name from 'updateLabel' to 'editLabel'.
    Change return value from status to json data.

--- back/src/controllers/label.controller.js
+++ back/src/controllers/label.controller.js
@@ -1,19 +1,18 @@
 const labelService = require('../services/label.service');
 
-//editLabel이기는 한데 database에서는 update가 어울려서 update라고 해둠..
-const updateLabel = async (req, res) => {
+const editLabel = async (req, res) => {
   try {
     const labelDatas = req.body;
     const result = await labelService.updateLabel(labelDatas);
     if (result) {
-      return res.status(200).send();
+      return res.json(result);
     }
-    return res.status(400).send();
+    return res.json({ result: false });
   } catch (err) {
     return res.status(500).send();
   }
 };
 
 module.exports = {
-  updateLabel,
+  editLabel,
 };
