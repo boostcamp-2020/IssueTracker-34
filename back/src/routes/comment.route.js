a2ee9390871f07c37d3f96836b0f508e7addaa7a
rockpell <aythffk@gmail.com>
2020-11-04 17:24:39 +0900

    Revert "[Feat] createComment"
    This reverts commit f1e248138d06169a7163381163b30c3d98b5f11a.

--- back/src/routes/comment.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/comment.controller');
-
-router.post('/', commentController.createComment);
-
-module.exports = router;
