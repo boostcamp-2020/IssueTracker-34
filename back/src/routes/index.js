0222a8bf35ff70cb0f0b37963be7ecb0f2c7c180
gyim1345 <gyim1345@gmail.com>
2020-11-04 23:27:17 +0900

    [Feat] Assignee API
    Add api for
    1. GET /assignee
    2. delete /assignee
    
    added validator for checking input existence
    add route, controller, service, model

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const label = require('./label.route');
+const assigneeRouter = require('./assignee.route');
+const assigneeValidator = require('../middleware/assigneeValidator');
 
 router.get('/', (req, res) => {
   res.send('hello');
 });
 
 router.use('/label', label);
+
+router.use('/assignee', assigneeValidator, assigneeRouter)
 
 module.exports = router;
