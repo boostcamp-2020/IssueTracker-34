f6d08fb5000a2762d4c10bf4e684cfa56816ccae
gyim1345 <gyim1345@gmail.com>
2020-11-04 23:55:44 +0900

    [Fix] typo
    change transactionService to labelService

--- back/src/controllers/label.controller.js
+++ back/src/controllers/label.controller.js
@@ -1,8 +1,8 @@
-const transactionService = require('../services/label.service');
+const labelService = require('../services/label.service');
 
 const getLabels = async (req, res) => {
   try {
-    const labels = await transactionService.getLabels();
+    const labels = await labelService.getLabels();
     return res.status(200).json(labels);
   } catch (err) {
     return res.status(500).json({ message: 'Internal Error' });
