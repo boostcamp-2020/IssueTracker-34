dae6002464ff99b01c6971c6b0b345bc630d12e1
profornnan <profornnan@naver.com>
2020-11-04 23:51:08 +0900

    [Feat] Use Issue Route
    Add issue router to index.js

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const label = require('./label.route');
+const issue = require('./issue.route');
 
 router.get('/', (req, res) => {
   res.send('hello');
 });
 
+router.use('/issue', issue);
 router.use('/label', label);
 
 module.exports = router;
