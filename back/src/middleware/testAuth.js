aefccad3d8adc0b74e6e5c778b38f5b5f908732f
rockpell <aythffk@gmail.com>
2020-11-04 17:24:14 +0900

    Revert "[Add] test code in index.js"
    This reverts commit 721646b9172efe8da07c4cf624a34717a7f6d2d5.

--- back/src/middleware/testAuth.js
+++
@@ -1,6 +0,0 @@
-const testAuth = function (req, res, next) {
-  req.body.userId = 1;
-  next();
-};
-
-module.exports = { testAuth };
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const label = require('./label.route');
-const comment = require('./comment.route');
-const { testAuth } = require('../middleware/testAuth');
 
 router.get('/', (req, res) => {
   res.send('hello');
 });
 
-router.use(testAuth);
 router.use('/label', label);
-router.use('/comment', comment);
 
 module.exports = router;
