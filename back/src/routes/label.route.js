682975684daca045cf049900ba86e1419691e1f4
gyim1345 <gyim1345@gmail.com>
2020-11-04 09:48:02 +0900

    [Feat] validate input middleware
    Add input validator for POST /label

--- back/src/routes/label.route.js
+++ back/src/routes/label.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const { validateCreateLabelInput } = require('../middleware/validateInputs')
 const labelController = require('../controllers/label.controller');
 
 router.get('/', (req, res) => {
   res.send('label');
 });
 
-router.post('/label', labelController.createLabel);
+router.post('/label', validateCreateLabelInput, labelController.createLabel);
 
 module.exports = router;
