4e3454b46e7f7cde87ad404b3f640fd5bf78acda
gyim1345 <gyim1345@gmail.com>
2020-11-05 12:56:04 +0900

    [Refactor] use env variable
    use dotenv for url variable

--- back/src/middleware/passport.js
+++ back/src/middleware/passport.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const githubPassport = passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/user/callback",
+  callbackURL: `${process.env.baseURL}/user/callback`,
 },
 async function(_, __, profile, done) {
   return done(null, profile);
