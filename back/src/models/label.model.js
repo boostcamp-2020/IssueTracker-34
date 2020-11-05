c1c2f95c4b0d852ae346c18a2be1cfb23282765e
rockpell <aythffk@gmail.com>
2020-11-05 13:33:59 +0900

    [Fix] createLabelModel
    remove try catch

--- back/src/models/label.model.js
+++ back/src/models/label.model.js
@@ -1,16 +1,11 @@
 const Label = require('../sequelizeModels/label.sequelizeModel');
 
 const createLabel = async ({ name, color, content }) => {
-  try {
-    return await Label.create({
-      name: name,
-      color: color,
-      content: content,
-    });
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
+  return await Label.create({
+    name: name,
+    color: color,
+    content: content,
+  });
 };
 
 module.exports = { createLabel };
