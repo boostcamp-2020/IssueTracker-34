edd93c3b749db4630c5d05afb37b155375d7e190
profornnan <profornnan@naver.com>
2020-11-06 10:09:40 +0900

    [Fix] Assignee Route
    Fix bug that can't import accountController module

--- back/src/routes/assignee.route.js
+++ back/src/routes/assignee.route.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const accountController = require('../controller/assignee.controller');
+const accountController = require('../controllers/assignee.controller');
 
 router.get('/', accountController.addAssignee);
-
 router.delete('/', accountController.deleteAssignee);
 
 module.exports = router;
