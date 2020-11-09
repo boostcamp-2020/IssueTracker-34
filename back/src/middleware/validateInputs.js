e5eb0f58e68949b9733e4083f8aa843b55319047
profornnan <profornnan@naver.com>
2020-11-09 16:16:32 +0900

    [Feat] Validate Edit Issue Input
    Add validateEditIssueInput middleware

--- back/src/middleware/validateInputs.js
+++ back/src/middleware/validateInputs.js
@@ -5,6 +5,22 @@ const validateCreateIssueInput = (req, res, next) => {
   next();
 };
 
+const validateEditIssueInput = (req, res, next) => {
+  const { title, statusOpenClosed } = req.body;
+  if (
+    !req.body.issueId ||
+    (!title && title !== undefined) ||
+    !(
+      statusOpenClosed === true ||
+      statusOpenClosed === false ||
+      statusOpenClosed === undefined
+    )
+  ) {
+    return res.status(400).send();
+  }
+  next();
+};
+
 const validateCreateLabelInput = (req, res, next) => {
   if (!req.body.name || !req.body.color) {
     return res.status(400).json('input(s) missing');
@@ -14,5 +30,6 @@ const validateCreateLabelInput = (req, res, next) => {
 
 module.exports = {
   validateCreateIssueInput,
+  validateEditIssueInput,
   validateCreateLabelInput,
 };
