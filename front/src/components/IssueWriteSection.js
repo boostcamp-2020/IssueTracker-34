2fb98cd3c3df1b930a2f50d3b6466b527f85f386
rockpell <aythffk@gmail.com>
2020-11-11 02:14:31 +0900

    [Refactor] IssueWriteSection
    separating Box component from IssueWriteSection

--- front/src/components/IssueWriteSection.js
+++ front/src/components/IssueWriteSection.js
@@ -3,39 +3,10 @@ import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import issueAPI from '../apis/issue.api';
 import Swal from 'sweetalert2';
+import Box from './boxes/SpeechBox';
 
 const defaultUserImageUrl =
   'https://Img.favpng.com/22/0/21/computer-icons-user-profile-clip-art-png-favpng-MhMHJ0Fw21MJadYjpvDQbzu5S.jpg';
-
-const Box = styled.div`
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 14%);
-  position: relative;
-  border-radius: 2px;
-  border: 1px #acc9ea solid;
-  width: 100%;
-  height: 100%;
-  :after {
-    content: ' ';
-    position: absolute;
-    left: -10px;
-    top: 10.5px;
-    border-top: 4.5px solid transparent;
-    border-right: 10px solid #ffffff4d;
-    border-bottom: 4.5px solid transparent;
-    z-index: 1;
-  }
-
-  :before {
-    content: ' ';
-    position: absolute;
-    left: -11px;
-    top: 10px;
-    border-top: 5px solid transparent;
-    border-right: 10px solid #acc9ea;
-    border-bottom: 5px solid transparent;
-    z-index: 0;
-  }
-`;
 
 const Header = styled.div`
   padding: 11px 5px 5px 5px;
