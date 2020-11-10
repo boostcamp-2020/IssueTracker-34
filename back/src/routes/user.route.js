1a80c571a762a49d68df1762cbddc0f8e26fa46d
gyim1345 <gyim1345@gmail.com>
2020-11-10 15:27:40 +0900

    [Add] token verifying validators
    Add token validators to verify token

--- back/src/routes/user.route.js
+++ back/src/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user.controller')
+const userController = require('../controllers/user.controller');
 const githubOauth = require('../middleware/githubOauth');
+const tokenValidator = require('../middleware/tokenValidator');
 
 router.post('/', githubOauth, userController.login);
 
-router.get('/', userController.getAllUsers)
+router.get('/', tokenValidator, userController.getAllUsers)
 
 module.exports = router;
