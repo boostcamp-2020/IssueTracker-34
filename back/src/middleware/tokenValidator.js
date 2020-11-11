adb50ba8d86b392e50c62e3aaf9c5ce098e864f0
gyim1345 <gyim1345@gmail.com>
2020-11-11 23:54:22 +0900

    [Fix] tokenValidator
    Revert back to previous status for development

--- back/src/middleware/tokenValidator.js
+++ back/src/middleware/tokenValidator.js
@@ -2,29 +2,29 @@ const JWTToken = require('../services/jwtToken.service');
 const UserModel = require('../models/user.model');
 
 module.exports = async function(req, res, next) {
-  try {
-    const [bearer, token] = req.headers.authorization.split(' ');
-    if (!token || token == null) {
-      return res.status(404).json('unauthorized');
-    }
-    const tokenInfo = JWTToken.verifyToken(token)
-    const user = await UserModel.findUser({ github_id: tokenInfo.data })
-    if (!user) {
-      return res.status(404).json('unauthorized');
-    }
-    req.body.authorizedUsername = user.name;
-    req.body.authorizedUserId = user.github_id;
-    next();
+  // try {
+  //   const [bearer, token] = req.headers.authorization.split(' ');
+  //   if (!token || token == null) {
+  //     return res.status(404).json('unauthorized');
+  //   }
+  //   const tokenInfo = JWTToken.verifyToken(token)
+  //   const user = await UserModel.findUser({ github_id: tokenInfo.data })
+  //   if (!user) {
+  //     return res.status(404).json('unauthorized');
+  //   }
+  //   req.body.authorizedUsername = user.name;
+  //   req.body.authorizedUserId = user.github_id;
+  //   next();
 
-  } catch (err) {
-    console.log(err)
-    return res.status(500).json({ message: err });
-  }
+  // } catch (err) {
+  //   console.log(err)
+  //   return res.status(500).json({ message: err });
+  // }
 
 
   // 개발용 임시 하드코딩
-  // req.body.authorizedUsername = 'profornnan';
-  // req.body.authorizedUserId = 1;
+  req.body.authorizedUsername = 'profornnan';
+  req.body.authorizedUserId = 1;
 
-  // next();
+  next();
 }
