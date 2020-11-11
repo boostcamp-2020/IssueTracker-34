f7b68e26ffee0e32f908ad2a00834b24cb8e477b
profornnan <profornnan@naver.com>
2020-11-11 23:39:20 +0900

    [Build] Change sequelize config
    Change settings to use pool in sequelize

--- back/src/sequelizeModels/config.sequelizeModels.js
+++ back/src/sequelizeModels/config.sequelizeModels.js
@@ -1,13 +1,17 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  dialect: process.env.SEQ_DIALECT,
+  pool: {
+    max: Number(process.env.SEQ_POOL_MAX),
+    min: Number(process.env.SEQ_POOL_MIN),
+    acquire: Number(process.env.SEQ_POOL_ACQUIRE),
+    idle: Number(process.env.SEQ_POOL_IDLE),
   },
-);
+});
 
 module.exports = sequelize;
