1ba85bd313b83f387acdc907112a96405a064e4c
gyim1345 <gyim1345@gmail.com>
2020-11-11 23:46:37 +0900

    [Feat] userInfo API
    Add API for userInfo
    Returns info of current user

--- back/src/middleware/tokenValidator.js
+++ back/src/middleware/tokenValidator.js
@@ -1,24 +1,30 @@
 const JWTToken = require('../services/jwtToken.service');
+const UserModel = require('../models/user.model');
 
-module.exports = function(req, res, next) {
-  // if (!token.headers.authorization) {
-  //   return res.status(404).json('unauthorized');
-  // }
+module.exports = async function(req, res, next) {
+  try {
+    const [bearer, token] = req.headers.authorization.split(' ');
+    if (!token || token == null) {
+      return res.status(404).json('unauthorized');
+    }
+    const tokenInfo = JWTToken.verifyToken(token)
+    const user = await UserModel.findUser({ github_id: tokenInfo.data })
+    if (!user) {
+      return res.status(404).json('unauthorized');
+    }
+    req.body.authorizedUsername = user.name;
+    req.body.authorizedUserId = user.github_id;
+    next();
 
-  // const [bearer, token] = req.headers.authorization.split(' ');
-  // const github_id = JWTToken.verify(token)
-  // const user = UserModel.findUser({ github_id })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ message: err });
+  }
 
-  // if (!user) {
-  //   return res.status(404).json('unauthorized');
-  // }
-
-  // req.body.authorizedUsername = user.username;
-  // req.body.authorizedUserId = user.id;
 
   // 개발용 임시 하드코딩
-  req.body.authorizedUsername = 'profornnan';
-  req.body.authorizedUserId = 1;
+  // req.body.authorizedUsername = 'profornnan';
+  // req.body.authorizedUserId = 1;
 
-  next();
+  // next();
 }
