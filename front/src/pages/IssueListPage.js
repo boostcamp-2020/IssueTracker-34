287dcca62006e6a539964c5fc3e2ed8d47a49817
profornnan <profornnan@naver.com>
2020-11-11 13:15:37 +0900

    [Refactor] Separate Reducer from IssueListPage
    Create reducers directory and IssueListPageReducer.js
    Separate reducer from IssueListPage

--- front/src/pages/IssueListPage.js
+++ front/src/pages/IssueListPage.js
@@ -8,6 +8,7 @@ import MilestonesButton from '../components/MilestonesButton';
 import NewIssueButton from '../components/NewIssueButton';
 import issueAPI from './../apis/issue.api';
 import userAPI from './../apis/user.api';
+import IssueListPageReducer from './../reducers/IssueListPageReducer';
 
 const FilterDiv = styled.div`
   display: flex;
@@ -27,62 +28,18 @@ const InnerFilterDiv = styled.div`
   flex-direction: row;
 `;
 
-const issueListReducer = (issueList, { type, payload }) => {
-  switch (type) {
-    case 'setInitial':
-      return payload
-        .map((issue) => {
-          return { ...issue, isChecked: false };
-        })
-        .sort((a, b) => (a.id > b.id ? -1 : 1));
-    case 'checkAll':
-      return issueList.map((issue) => {
-        return { ...issue, isChecked: payload.isChecked };
-      });
-    case 'check':
-      return issueList.map((issue) => {
-        return issue.id === payload.id
-          ? { ...issue, isChecked: !issue.isChecked }
-          : issue;
-      });
-  }
-};
-
-const authorListReducer = (authorList, { type, payload }) => {
-  switch (type) {
-    case 'setInitial':
-      return payload
-        .map((author) => {
-          return { ...author, isChecked: false };
-        })
-        .sort((a, b) => (a.name > b.name ? 1 : -1));
-    case 'check':
-      const [checkedAuthor] = authorList.filter((author) => author.isChecked);
-      if (checkedAuthor !== undefined && checkedAuthor.id === payload.id)
-        return authorList.map((author) => {
-          return { ...author, isChecked: false };
-        });
-      return authorList
-        .map((author) => {
-          return {
-            ...author,
-            isChecked: author.id === payload.id ? true : false,
-          };
-        })
-        .sort((a, b) => {
-          if (a.isChecked) return -1;
-          if (b.isChecked) return 1;
-          return a.name > b.name ? 1 : -1;
-        });
-  }
-};
-
 export const IssueListContext = createContext();
 export const AuthorListContext = createContext();
 
 const IssueListPage = () => {
-  const [issueList, issueListDispatch] = useReducer(issueListReducer, []);
-  const [authorList, authorListDispatch] = useReducer(authorListReducer, []);
+  const [issueList, issueListDispatch] = useReducer(
+    IssueListPageReducer.issueListReducer,
+    []
+  );
+  const [authorList, authorListDispatch] = useReducer(
+    IssueListPageReducer.authorListReducer,
+    []
+  );
 
   useEffect(async () => {
     const issues = await issueAPI.getIssues();
---
+++ front/src/reducers/IssueListPageReducer.js
@@ -0,0 +1,52 @@
+const IssueListPageReducer = {
+  issueListReducer(issueList, { type, payload }) {
+    switch (type) {
+      case 'setInitial':
+        return payload
+          .map((issue) => {
+            return { ...issue, isChecked: false };
+          })
+          .sort((a, b) => (a.id > b.id ? -1 : 1));
+      case 'checkAll':
+        return issueList.map((issue) => {
+          return { ...issue, isChecked: payload.isChecked };
+        });
+      case 'check':
+        return issueList.map((issue) => {
+          return issue.id === payload.id
+            ? { ...issue, isChecked: !issue.isChecked }
+            : issue;
+        });
+    }
+  },
+  authorListReducer(authorList, { type, payload }) {
+    switch (type) {
+      case 'setInitial':
+        return payload
+          .map((author) => {
+            return { ...author, isChecked: false };
+          })
+          .sort((a, b) => (a.name > b.name ? 1 : -1));
+      case 'check':
+        const [checkedAuthor] = authorList.filter((author) => author.isChecked);
+        if (checkedAuthor !== undefined && checkedAuthor.id === payload.id)
+          return authorList.map((author) => {
+            return { ...author, isChecked: false };
+          });
+        return authorList
+          .map((author) => {
+            return {
+              ...author,
+              isChecked: author.id === payload.id ? true : false,
+            };
+          })
+          .sort((a, b) => {
+            if (a.isChecked) return -1;
+            if (b.isChecked) return 1;
+            return a.name > b.name ? 1 : -1;
+          });
+    }
+  },
+};
+
+export default IssueListPageReducer;
