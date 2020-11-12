5a5360f5abe3ab5e8241331762d6648c55a64e86
gyim1345 <gyim1345@gmail.com>
2020-11-13 03:47:00 +0900

    [Fix] issue create
    Find userId using github id and then create

--- back/src/services/issue.service.js
+++ back/src/services/issue.service.js
@@ -1,8 +1,11 @@
 const issueModel = require('../models/issue.model');
+const UserModel = require("../models/user.model");
 
 const issueService = {
-  async createIssue(data) {
-    return await issueModel.createIssue(data);
+  async createIssue({ userId, title, content, milestone, labels, assignees, authorizedUserId }) {
+    const userDB_ID = await UserModel.findUser({ github_id: authorizedUserId })
+
+    return await issueModel.createIssue({ userId: userDB_ID.dataValues.id, title, content, milestone, labels, assignees });
   },
   async getIssues() {
     const issues = await issueModel.getIssues();
